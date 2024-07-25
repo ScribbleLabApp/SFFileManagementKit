10591a69903fbf33b7adaae6ab907d47953f16b7
N3v1 <n3v1010@gmail.com>
2024-07-25 14:06:48 +0200

    (feat): keychh

--- Sources/SFFileCore/include/libc/keychh.h
+++ Sources/SFFileCore/include/libc/keychh.h
@@ -1,9 +1,26 @@
-//
-//  Header.h
-//  SFFileManagementKit
-//
-//  Created by Nevio Hirani on 25.07.24.
-//
+//===-- SFFileCore/include/libc/keychh.h - Keychain operations -  -*- C -*-===//
+//                                                                            //
+// This source file is part of the Scribble Foundation open source project    //
+//                                                                            //
+// Copyright (c) 2024 ScribbleLabApp. and the ScribbleLab project authors     //
+// Licensed under Apache License v2.0 with Runtime Library Exception          //
+//                                                                            //
+// You may not use this file except in compliance with the License.           //
+// You may obtain a copy of the License at                                    //
+//                                                                            //
+//      http://www.apache.org/licenses/LICENSE-2.0                            //
+//                                                                            //
+// Unless required by applicable law or agreed to in writing, software        //
+// distributed under the License is distributed on an "AS IS" BASIS,          //
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   //
+// See the License for the specific language governing permissions and        //
+// limitations under the License.                                             //
+//                                                                            //
+//===----------------------------------------------------------------------===//
+///
+/// \file
+///
+//===----------------------------------------------------------------------===//
 
 #ifndef keychh_h
 #define keychh_h
